610bedfaac3bff79c3ae8d33e4d0f087a3364e5a
baohuydz <168200545+baohuydz@users.noreply.github.com>
2026-05-28 03:38:15 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,6 @@
-import os
 from flask import Flask
-
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hệ thống Siêu Trí Tuệ BaoHuyDevs đang hoạt động 24/7 ổn định trên Render!"
-
-def setup_keep_alive():
-    # Lấy cổng động do Render cấp qua biến môi trường 'PORT'
-    port = int(os.environ.get("PORT", 8080))
-    return port
+    return "Hệ thống Siêu Trí Tuệ BaoHuyDevs hoạt động tốt!"
